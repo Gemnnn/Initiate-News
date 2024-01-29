8d648c2d145320857be2fd86c4f7675895edfa20
Branden Min <brandenmin@gmail.com>
2024-01-28 23:01:06 -0500

    feat: add login front-end

---
+++ app/src/main/java/com/initiatetech/initiate_news/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.initiatetech.initiate_news.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.initiatetech.initiate_news.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
