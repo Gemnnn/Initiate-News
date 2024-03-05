1ab36e3f3575ac489cd8ba3b2f6051d30aaf10dc
Branden Min <brandenmin@gmail.com>
2024-03-04 23:07:15 -0500

    feat: add splash screen

--- app/src/main/java/com/initiatetech/initiate_news/SplashActivity.kt
+++ app/src/main/java/com/initiatetech/initiate_news/SplashActivity.kt
@@ -3,21 +3,60 @@ package com.initiatetech.initiate_news
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
 import androidx.appcompat.app.AppCompatActivity
+import com.initiatetech.initiate_news.databinding.ActivitySplashBinding
 import com.initiatetech.initiate_news.login.LoginActivity
 
-
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val texts = arrayOf("Simplified", "Stay Informed", "Personalized")
+    private var textIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        Handler().postDelayed(Runnable {
+        Handler(Looper.getMainLooper()).postDelayed({
+        }, 500) // 300 milliseconds delay
 
-            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+        animateText()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000) // The delay
-
+        }, 4000) // 3.5 seconds delay
     }
+
+    private fun animateText() {
+        val handler = Handler(Looper.getMainLooper())
+        val runnable = object : Runnable {
+            override fun run() {
+                if (textIndex < texts.size) {
+                    binding.textViewAnimated.apply {
+                        startAnimation(fadeInAnimation)
+                        text = texts[textIndex++]
+                        startAnimation(fadeOutAnimation)
+                    }
+                    handler.postDelayed(this, 1500) // Delay to accommodate animations
+                }
+            }
+        }
+        handler.post(runnable)
+    }
+
+    private val fadeInAnimation: Animation = AlphaAnimation(0.0f, 1.0f).apply {
+        duration = 1100
+        fillAfter = true
+    }
+
+    private val fadeOutAnimation: Animation = AlphaAnimation(1.0f, 0.0f).apply {
+        duration = 1100
+        fillAfter = true
+    }
+
 }
