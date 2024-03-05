43a6a7f7df7244e306b1e4cbbd7db31cf78354e7
Brendan Donnachie <brendandonnachie99@gmail.com>
2024-03-05 18:12:54 -0500

    feat: Added api call to generate news in the db for a new keyword

--- app/src/main/java/com/initiatetech/initiate_news/repository/NewsRepository.kt
+++ app/src/main/java/com/initiatetech/initiate_news/repository/NewsRepository.kt
@@ -3,7 +3,10 @@ package com.initiatetech.initiate_news.repository
 import com.initiatetech.initiate_news.api.RetrofitClient
 
 class NewsRepository {
+    // Summarized news
     fun getAllKeywordNews(username: String, keyword: String) = RetrofitClient.instance.getAllKeywordNews(username, keyword)
-    fun getNewsDetail(username: String, newsId: Int) = RetrofitClient.instance.getKeywordNews(username, newsId)
+    fun getFirstKeywordNews(username: String, keyword: String) = RetrofitClient.instance.getFirstKeywordNews(username, keyword)
 
+    // News
+    fun getNewsDetail(username: String, newsId: Int) = RetrofitClient.instance.getKeywordNews(username, newsId)
 }
